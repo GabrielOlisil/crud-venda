bb2afea43b61cf09f0afcda9b4afedb6515fd085
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2024-08-13 21:14:58 -0400

    primeiro commit

---
+++ CrudVenda/Control/Menu.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudVenda.Control;
+
+class Menu
+{
+    private readonly MenuItem[] _menuItems;
+    private int _indexMarked = 0;
+
+    public Menu(MenuItem[] menuItems)
+    {
+        _menuItems = menuItems;
+        _menuItems[0].IsMarked = true;
+    }
+
+    private void Render()
+    {
+        Console.Clear();
+        foreach (var i in _menuItems)
+        {
+            Console.ResetColor();
+            if (i.IsMarked)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine(i.Name);
+                Console.ResetColor();
+                continue;
+            }
+            Console.WriteLine(i.Name);
+        }
+    }
+
+    private MenuItem? SetItem(ConsoleKeyInfo tecla)
+    {
+
+        if (tecla.Key == ConsoleKey.DownArrow)
+        {
+
+            if (_indexMarked == _menuItems.Length - 1)
+            {
+                return null;
+            }
+            _menuItems[_indexMarked].IsMarked = false;
+            _menuItems[++_indexMarked].IsMarked = true;
+
+            Render();
+            return null;
+        }
+
+
+
+        if (tecla.Key == ConsoleKey.UpArrow)
+        {
+
+            if (_indexMarked == 0)
+            {
+                return null;
+            }
+            _menuItems[_indexMarked].IsMarked = false;
+            _menuItems[--_indexMarked].IsMarked = true;
+            Render();
+            return null;
+        }
+
+        if (tecla.Key != ConsoleKey.Enter)
+        {
+            return null;
+        }
+
+        return _menuItems[_indexMarked];
+    }
+
+    public MenuItem GetOption()
+    {
+        Render();
+
+        var key = Console.ReadKey();
+        var escolha = SetItem(key);
+
+        while (escolha is null)
+        {
+            key = Console.ReadKey();
+            escolha = SetItem(key);
+        }
+
+        return escolha;
+    }
+}
---
+++ CrudVenda/Program.cs
@@ -0,0 +1,18 @@
+﻿using CrudVenda.Control;
+
+
+var menuitens = new MenuItem[]
+{
+    new MenuItem { Name = "test" },
+    new MenuItem { Name = "test2" },
+    new MenuItem { Name = "test3" },
+    new MenuItem { Name = "test4" },
+    new MenuItem { Name = "test5" }
+};
+
+Menu menu = new Menu(menuitens);
+
+var op = menu.GetOption();
+
+
+Console.WriteLine("Voce escolheu esta opção:" + op.Name);
